cd88b9e33f796ecd3be0f0c013909c409d65c470
Michael Achumba <michaelachumba@gmail.com>
2023-08-28 13:27:50 +0100

    Temprature conversion table with for loop

---
+++ c_programming_kandr/chap_1/ex1_5-fahr_cel_table.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints Fahrenheit-Celsius table
+ * for fahr = 300, 280, ..., 0
+ * Return: 0
+ */
+int main(void)
+{
+	int fahr;
+
+	for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+	{
+		printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
+	}
+	return (0);
+}
